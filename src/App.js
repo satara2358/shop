7906daa13c149c40216c5d2fa67da116595c75c6
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-01-29 17:20:56 -0500

    Creacion de proyecto aula

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { Tooltip, TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+// import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+// import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 import './App.css';
 
-function App() {
+export const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='flex relative dark:bg-main-dark-bg'> 
+        <div className='fixed right-4 bottom-4' style={{ zIndex: '1000'}}>
+          <TooltipComponent content="Settings" position='Top'>
+            <button
+              type='button'
+              className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
+              style={{background: 'blue', borderRadius: '50%' }}
+            >
+              <FiSettings/>
+            </button>
+          </TooltipComponent>
+        </div>
+      </div>
+    
+    </BrowserRouter>
   );
-}
-
-export default App;
+};
